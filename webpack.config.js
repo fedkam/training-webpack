03a62b84f411c067ceb59909905779501021fcd1
fed <korolevfedos@yandex.ru>
2020-08-20 17:01:48 +1200

    50:23

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin}=require('clean-webpack-plugin')
 
 module.exports = {
     mode: 'development',
@@ -12,6 +13,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
-      new HTMLWebpackPlugin({template: './src/index.html'})
+      new HTMLWebpackPlugin({template: './src/index.html'}),
+      new CleanWebpackPlugin()
     ]
 };
