07c930a0b925f37217be669e21bd7abcad3c8ff3
fed <korolevfedos@yandex.ru>
2020-08-21 12:14:22 +1200

    1:13:

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import Post from './Post';
+import json from './assets/json';
 import './styles/styles.css';
+import Webpacklogo from './assets/webpack-logo.png';
 
-const post = new Post('Webpack post title');
+const post = new Post('Webpack post title', Webpacklogo);
 
 console.log('post to string:', post.toString());
+console.log('json:', json);
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 module.exports = {
     context: path.resolve(__dirname, 'src'), //где лежат иходники
     mode: 'development', //тип сбора
-    entry: { //файлы
+    entry: { //файлы (чанки)
         main: './index.js', 
         analytics: './analitics.js'
     },
@@ -17,11 +17,19 @@ module.exports = {
         new HTMLWebpackPlugin({ template: './index.html' }), //добавление к сборке html с подкл(с [name].[contentHash]) javascript
         new CleanWebpackPlugin() //очистка папки сборки от неактульных файлов
     ],
-    module:{
+    module:{ //лоадеры (доп.конф)
         rules: [
-            {
+            { //для загрузки css
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader'] //1)добавление в head html, 2)обработка import style
+                use: ['style-loader', 'css-loader'] //1)добавление в head для html, 2)обработка import style в js файлах
+            },
+            { //для изображений
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            },
+            { //для шрифтов
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader']
             }
         ]
     }
